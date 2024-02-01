dd9171250bf20ed05d58ccd87e40e3584fd95744
YashPimpalkar <62347457+YashPimpalkar@users.noreply.github.com>
2024-02-01 20:01:38 +0530

    Update verifyToken.js

--- TourManagement/backend/utils/verifyToken.js
+++ TourManagement/backend/utils/verifyToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.token;
-    console.log( req.cookies.accesstoken);
+    console.log( req.cookies.token);
     if (!token) {
         return res.status(401).json({ success: false, message: "You're not autherize" })
     }
